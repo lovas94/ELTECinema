ecf980c3344fbc37d28fc7bfe72d1ceed67bc663
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-04-25 23:30:11 +0200

    add User Role annotation

---
+++ backend/src/main/java/projeszk/eltecinema/annotation/Role.java
@@ -0,0 +1,14 @@
+package projeszk.cinema.annotation;
+
+import projeszk.cinema.model.User;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Role {
+    User.Role[] value() default { User.Role.GUEST };
+}
