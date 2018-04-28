17ddd96bb4d0670f738efe67106a38a75622e3e3
starlord0302 <starlord0302@users.noreply.github.com>
2018-04-28 20:15:20 +0200

    add dao

---
+++ backend/src/main/java/projeszk/eltecinema/dao/DaoInterface.java
@@ -0,0 +1,14 @@
+package projeszk.eltecinema.dao;
+
+import java.util.List;
+
+public interface DaoInterface<Entity> {
+
+    void updateEntity(Entity entity);
+    void deleteEntity(Entity entity);
+    void deleteEntityById(Integer id);
+    boolean insertEntity(Entity entity);
+    Entity findEntity(Integer id);
+    boolean exists(Entity entity);
+    List<Entity> getEntities();
+}
