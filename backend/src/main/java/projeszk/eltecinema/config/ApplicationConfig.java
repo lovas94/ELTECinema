c26131e9ae49567b2f72d7e0e914ddc72ef50b21
robence <robence@gmail.com>
2018-04-29 21:41:33 +0200

    added configs databse and application

---
+++ backend/src/main/java/projeszk/eltecinema/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package projeszk.eltecinema.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({DatabaseConfig.class, DaoConfig.class})
+public class ApplicationConfig {
+
+
+}
